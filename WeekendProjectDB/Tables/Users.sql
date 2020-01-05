6b6a17b586b6158cfb208faa7d4912868abfe71a
vikas <vikas32567@gmail.com>
2020-01-05 11:59:45 +0530

    Uploading local files

---
+++ WeekendProjectDB/Tables/Users.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [Username] NCHAR(50) NOT NULL, 
+    [Email] NCHAR(50) NOT NULL, 
+    [Password] NCHAR(50) NOT NULL, 
+    [Salt] NCHAR(50) NOT NULL, 
+    [VerifyToken] NCHAR(50) NULL, 
+    [ResetToken] NCHAR(50) NULL, 
+    [Created] DATETIME NOT NULL DEFAULT GETDATE(), 
+    [Modified] DATETIME NULL
+)
